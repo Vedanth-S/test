afa139a195b735c839d3fc24f6f4aa84d6a5d201
Vedanth Sriram <vedanthsriram15@gmail.com>
2021-05-07 16:21:26 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+word=input("Enter a word: ")
+if (word[0].isalpha()==True) and ((word[0] in "bcdfghjklmnpqrstvwxyz") or (word[0] in "BCDFGHJKLMNPQRSTVWXYZ")==True):
+        print("The Pig latin equivalent of the word is",word[1:len(word)+1]+word[0]+"ay")
+else:
+    print("The Pig latin equivalent of the word is",word+"yay")
+
+
+            
+    
